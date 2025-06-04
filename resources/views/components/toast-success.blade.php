bb66295d418e1586af0a92baa19306d36c18a50d
Hendra <pthendrasetiawan@gmail.com>
2025-06-04 19:12:29 +0800

    Fix toast

--- resources/views/components/toast-success.blade.php
+++ resources/views/components/toast-success.blade.php
@@ -1,4 +1,4 @@
 <div id="toast"
     class="fixed bottom-5 right-5 bg-[#00a63e] text-white px-4 py-3 rounded-lg shadow-lg transition-opacity duration-300">
-    {{ session('success') }}
+    {{ $message }}
 </div>
