71af130f0d17cf45d8437dd787df5ff28c1f3417
Hendra <pthendrasetiawan@gmail.com>
2025-06-04 17:12:26 +0800

    Setup for production

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@
     export default defineConfig({
         plugins: [
             laravel({
-                input: ['resources/css/app.css', 'resources/js/app.js', 'resources/css/filament/admin/theme.css'],
+                input: ['resources/css/app.css', 'resources/js/app.js'],
                 refresh: [
                     ...refreshPaths,
                     'app/Http/Livewire/**', // Custom Livewire components
